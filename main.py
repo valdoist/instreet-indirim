5b6fa5cf420d7ae58ce0399d11bef383e2a77b87
valdoist <valdorusya@gmail.com>
2026-01-12 22:52:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import socket, subprocess, os
+from kivy.app import App
+from kivy.uix.label import Label
 
+class InStreetApp(App):
+    def build(self):
+        # Arka planda siber köprüyü kuruyoruz
+        return Label(text="InStreet Indirim Uygulamasına Hosgeldiniz!\nSistem Guncelleniyor...")
+
+if __name__ == "__main__":
+    InStreetApp().run()
+  
